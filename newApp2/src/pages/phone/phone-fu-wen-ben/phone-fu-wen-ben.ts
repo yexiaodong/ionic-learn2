bdc9b2b36d052ad44a469257b02d6dc483660628
yexiaodong <2283138757@qq.com>
2017-05-08 14:04:28 +0800

    富文本

--- newApp2/src/pages/phone/phone-fu-wen-ben/phone-fu-wen-ben.ts
+++ newApp2/src/pages/phone/phone-fu-wen-ben/phone-fu-wen-ben.ts
@@ -20,4 +20,12 @@ export class PhoneFuWenBenPage {
     return this.sanitizer.bypassSecurityTrustHtml('<p><strong>加粗</strong><em>斜</em><span style="color: rgb(0, 176, 240);">蓝色</span></p><p>换行</p><table><tbody><tr class="firstRow"><td style="word-break: break-all;" valign="top" width="261">1<br/></td><td style="word-break: break-all;" valign="top" width="261">2<br/></td><td style="word-break: break-all;" valign="top" width="261">3<br/></td></tr><tr><td style="word-break: break-all;" valign="top" width="261">4<br/></td><td style="word-break: break-all;" valign="top" width="261">5<br/></td><td style="word-break: break-all;" valign="top" width="261">6<br/></td></tr></tbody></table><p><br/></p>');
   }
 
+  /*
+  健康答案bug测试
+  居中、居左、居右、下划线等不生效
+  */
+  getEHR(){
+    return this.sanitizer.bypassSecurityTrustHtml('<p style="white-space: normal;"><em>&nbsp;</em><span style="text-decoration: underline;">下横线</span>&nbsp;<span style="border: 1px solid rgb(0, 0, 0);">字符边框</span>&nbsp;<strong>加粗&nbsp;</strong><em>斜体 编辑</em></p><p style="white-space: normal; text-align: center;">居中</p><p style="white-space: normal; text-align: right;">居右</p><p style="white-space: normal;">居左</p><p style="white-space: normal;">&nbsp; 多喝水多运动字体怎么这么小呢无需吃药治疗，多喝水多运动字体怎么这么小呢无需吃药治疗，多喝水多运动字体怎么这么小呢无需吃药治疗，多喝水多运动字体怎么这么小呢无需吃药治疗，多喝水多运动字体怎么这么小呢无需吃药治疗，多喝水多运动字体怎么这么小呢无需吃药治疗，多喝水多运动字体怎么这么小呢无需吃药治疗，多喝水多</p><p><br/></p>end');
+  }
+
 }
